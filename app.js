e683b2e22173b9cf950ec739c4aed03fefb3e80d
Alejandra Herrera <alejandrah582@gmail.com>
2023-04-19 13:54:16 -0700

    updated js file

--- app.js
+++ app.js
@@ -1,69 +1,65 @@
-class Person {
-    //creates instance of Person with properties name, age, and numBooksRead
-    constructor(name, age, numBooksRead) {
-        this.name = name;
-        this.age = age;
-        this.numBooksRead = numBooksRead;
+class Music {
+
+    constructor(artist, genre, numPlayBacks) {
+        this.artist = artist;
+        this.genre = genre;
+        this.numPlayBacks = numPlayBacks;
     }
 
-    //increments numBooksRead by 1
-    readNewBook() {
-        this.numBooksRead++;
-    }
-}
-
-class Electrician extends Person {
-    //creates instance of Electrician with Person properites plus certifications, an array of strings
-    constructor(name, age, numBooksRead, certifications) {
-        super(name, age, numBooksRead);
-        this.certifications = certifications;
+   
+    newSong() {
+        this.numPlayBacks++;
     }
 }
 
-class Teenager extends Person {
-    //creates instance of Teenager with Person properties plus isHungry, a boolean value
-    constructor(name, age, numBooksRead, isHungry = true) {
-        super(name, age, numBooksRead);
-        this.isHungry = isHungry;
+class Indie extends Music {
+    
+    constructor(artist, genre, numPlayBacks, awards) {
+        super(artist, genre, numPlayBacks);
+        this.awards = awards;
+    }
+}
+
+class RnB extends Music {
+    constructor(artist, genre, numPlayBacks, isBanger = true) {
+        super(artist, genre, numPlayBacks);
+        this.isBanger = isBanger;
     }
 
-    //feeds the teenager.  If they are hungry, changes isHungry to false, if they are not hungry, prints message to console.
-    eat() {
-        if(this.isHungry) {
-            this.isHungry = false;
+    
+    sing () {
+        if(this.isBanger) {
+            this.isBanger = false;
         } else {
-            console.log('Oh no I think I ate too much')
+            console.log('Change the song!')
         }
     }
 }
 
-//below we will test our classes by instantiating them, calling each method, and verifying output
 
-//create instances of each class
-let person = new Person('Euthyphro', 35, 0);
-let electrician = new Electrician('Zeus', 28, 3, ['Lightning', 'Polymorphism']);
-let teen = new Teenager('Persius', 16, 5, true);
+let music = new Music('Sza', 35, 0);
+let indie = new Indie('Omar Apollo', 28, 3, ['Kali Uchis', 'Tyler the Creator']);
+let rnB = new RnB('Victoria Monet', 16, 5, true);
 
-//log each instance
-console.log(person);
-console.log(electrician);
-console.log(teen);
 
-//call the method from the parent class to ensure it works as expected
-person.readNewBook();
-electrician.readNewBook();
-teen.readNewBook();
+console.log(music);
+console.log(indie);
+console.log(rnB);
 
-//log the objects after calling .readNewBook() to verify it worked as intended
-console.log(person);
-console.log(electrician);
-console.log(teen);
 
-//call .eat()
-teen.eat();
+music.newSong();
+indie.newSong();
+rnB.newSong();
 
-//check that .eat() when isHungry == true works as expected
-console.log(teen);
 
-//check that .eat() when isHungry == false works as expected
-teen.eat();
+console.log(music);
+console.log(indie);
+console.log(rnB);
+
+
+rnB.sing();
+
+
+console.log(rnB);
+
+rnB.sing();
